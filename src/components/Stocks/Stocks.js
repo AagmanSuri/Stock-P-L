67eb00c9ca935c2eb4e4bab10ff2d38038a54801
Aagman Suri <aagmansuri96@gmail.com>
2021-08-25 13:00:15 +0530

    form created

--- src/components/Stocks/Stocks.js
+++ src/components/Stocks/Stocks.js
@@ -1,12 +1,27 @@
 import React from "react";
 
 const Stocks = () => {
+  const submitFun = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      <h1>Stocks </h1>
-      Amont : <input type="Number" placeholder="Amt in nos"></input>
-      <br></br>
-      Quantity : <input type="Number" placeholder="Amt in nos"></input>
+      <h1>Stocks</h1>
+      <form onSubmit={submitFun}>
+        <label htmlFor="buy">Stock Buy Price : </label>
+        <input id="buy"></input>
+        <br></br>
+        <br></br>
+        <label htmlFor="currentPrice">Stock Current Price : </label>
+        <input id="currentPrice"></input>
+        <br></br>
+        <br></br>
+        <label htmlFor="qty">Qty baught : </label>
+        <input id="qty"></input>
+        <br></br>
+        <br></br>
+        <button type="submit">Calculate P/L</button>
+      </form>
     </div>
   );
 };

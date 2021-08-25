9373952fa3b9532e28da7855f57fc73d42f15f40
Aagman Suri <aagmansuri96@gmail.com>
2021-08-25 13:17:22 +0530

    calculation

--- src/components/Stocks/Stocks.js
+++ src/components/Stocks/Stocks.js
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Stocks = () => {
+  const [buyPrice, setBuyPrice] = useState(0);
+  const [currPrice, setCurrPrice] = useState(0);
+  const [qty, setqty] = useState(0);
   const submitFun = (e) => {
     e.preventDefault();
+    alert(`buy price ${buyPrice}, ${currPrice},${qty}`);
   };
+
   return (
     <div>
-      <h1>Stocks</h1>
+      <h1>Stocks Profit/Loss</h1>
       <form onSubmit={submitFun}>
         <label htmlFor="buy">Stock Buy Price : </label>
-        <input id="buy"></input>
+        <input
+          id="buy"
+          name="buy"
+          value={buyPrice}
+          onChange={(e) => setBuyPrice(e.target.value)}
+        ></input>
         <br></br>
         <br></br>
         <label htmlFor="currentPrice">Stock Current Price : </label>
-        <input id="currentPrice"></input>
+        <input
+          id="currentPrice"
+          name="currentPrice"
+          value={currPrice}
+          onChange={(e) => setCurrPrice(e.target.value)}
+        ></input>
         <br></br>
         <br></br>
         <label htmlFor="qty">Qty baught : </label>
-        <input id="qty"></input>
+        <input
+          id="qty"
+          name="qty"
+          value={qty}
+          onChange={(e) => setqty(e.target.value)}
+        ></input>
         <br></br>
         <br></br>
         <button type="submit">Calculate P/L</button>
       </form>
+      <div>
+        <h1> the net is {(currPrice - buyPrice) * qty}</h1>
+      </div>
     </div>
   );
 };
